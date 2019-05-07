660b5aabc3a95c13640db7c962178931b729482f
ulmers <stephenulmer@knights.ucf.edu>
2019-05-07 19:29:53 -0400

    oauth init

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports.setConfig = function(){
     process.env.MONGOOSE_CONNECT = "mongodb://localhost:27017/test"
     process.env.SECRET_KEY = "mykey"
+    process.env.CLIENT_ID = '615403106003.620521868369';
+    process.env.CLIENT_SECRET = 'f700f2c409f0ea70b944a4e2c683efe3';
 }
--- src/controllers/data-controller.js
+++ src/controllers/data-controller.js
@@ -68,8 +68,7 @@ module.exports.oauth = (req, res) => {
         uri: 'https://slack.com/api/oauth.access?code='
             +req.query.code+
             '&client_id='+process.env.CLIENT_ID+
-            '&client_secret='+process.env.CLIENT_SECRET+
-            '&redirect_uri='+process.env.REDIRECT_URI,
+            '&client_secret='+process.env.CLIENT_SECRET,
         method: 'GET'
     };
 
